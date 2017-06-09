f6ba1f5831050e85f72739065b29a183d69628d4
Anubhav Ashok <anubhava@andrew.cmu.edu>
2017-06-09 00:13:32 -0400

    Made vgg fit on gpu

--- models/vgg_twostream.py
+++ models/vgg_twostream.py
@@ -8,11 +8,18 @@ from config import *
 class TwoStreamNetwork(nn.Module):
     def __init__(self):
         super(TwoStreamNetwork, self).__init__()
-        model = models.vgg16(pretrained=True)
-        model.classifier._modules['6'] = nn.Linear(4096, NUM_ACTIONS)
+        model = models.vgg11(pretrained=True)
+        model.features._modules['2'] = nn.MaxPool2d((4, 4), stride=(4, 4), dilation=(1, 1))
+        model.classifier._modules['0'] = nn.Linear(4608, 512)
+        model.classifier._modules['6'] = nn.Linear(512, FEATURE_SIZE)
+        del model.classifier._modules['2']
+        del model.classifier._modules['3']
+        del model.classifier._modules['4']
+        #del model.classifier._modules['5']
+        #del model.classifier._modules['6']
         self.RGBStream = deepcopy(model)
         self.FlowStream = deepcopy(model)
-        self.FlowStream.features._modules['0'] = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.FlowStream.features._modules['0'] = nn.Conv2d(6, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
         resetModel(self.FlowStream)
         #self.pool = nn.AvgPool1d(8)
         self.rgbdropout = nn.Dropout()
