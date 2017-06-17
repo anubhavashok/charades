bd490a15736cf104a6a3e62eeb26967ddaf47a61
Anubhav Ashok <anubhava@andrew.cmu.edu>
2017-06-16 21:19:56 -0400

    Made changes to rgb-only and added flow model

--- models/flow_vgg16_twostream_lstm.py
+++ models/flow_vgg16_twostream_lstm.py
@@ -9,12 +9,14 @@ from config import *
 class TwoStreamNetworkLSTM(nn.Module):
     def __init__(self):
         super(TwoStreamNetworkLSTM, self).__init__()
-        model = torch.load('models/pretrained_rgb.net')#models.vgg16(pretrained=True)
-        model.classifier._modules['6']=nn.Linear(4096, FEATURE_SIZE)
-        self.RGBStream = model
+        model = models.vgg16(pretrained=True)
+        model.classifier._modules['6'] = nn.Linear(4096, FEATURE_SIZE)
+        self.FlowStream = model
+        self.FlowStream.features._modules['0'] = nn.Conv2d(6, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        #resetModel(self.FlowStream)
         self.hidden_size = HIDDEN_SIZE
         self.lstm = torch.nn.LSTM(FEATURE_SIZE, self.hidden_size, 2, bidirectional=True)
-        self.rgbdropout = nn.Dropout(0.5)
+        self.flowdropout = nn.Dropout(0.5)
         self.pool = nn.AvgPool1d(8)
 
     def reset_hidden(self, batch=1):
@@ -23,9 +25,9 @@ class TwoStreamNetworkLSTM(nn.Module):
     def forward(self, rgb, flow):
         # Since we are batching sequences we can reset each time
         self.reset_hidden()
-        rgbout = self.RGBStream(rgb)
-        rgbout = self.rgbdropout(rgbout)
-        feat = rgbout
+        flowout = self.FlowStream(flow)
+        flowout = self.flowdropout(flowout)
+        feat = flowout
         feat = feat.unsqueeze(1)
         #feat = self.pool(feat)
         out, self.h = self.lstm(feat, self.h)
