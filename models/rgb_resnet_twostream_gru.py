24312110ac3d0c2cee6542809508b9d3c48a7ab5
Anubhav Ashok <anubhava@andrew.cmu.edu>
2017-07-12 23:15:01 -0400

    Added gru version

---
+++ models/rgb_resnet_twostream_gru.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from copy import deepcopy
+from torchvision import models
+from utils import *
+from config import *
+
+class TwoStreamNetworkLSTM(nn.Module):
+    def __init__(self):
+        super(TwoStreamNetworkLSTM, self).__init__()
+        print('Model: RGB/RESNET/GRU')
+        USE_FLOW=False
+        model = models.resnet18(pretrained=True)
+        model.fc = nn.Linear(512, FEATURE_SIZE)
+        self.RGBStream = model
+        self.hidden_size = HIDDEN_SIZE
+        self.lstm = torch.nn.GRU(FEATURE_SIZE, self.hidden_size, 2, bidirectional=True, dropout=0.5)
+        self.rgbdropout = nn.Dropout(0.5)
+        self.pool = nn.AvgPool1d(4)
+
+    def reset_hidden(self, batch=1):
+        self.h = Variable(torch.zeros(2*2, batch, self.hidden_size)).cuda()
+
+    def forward(self, rgb, flow, reset=True):
+        if reset:
+            # Since we are batching sequences we can reset each time
+            self.reset_hidden()
+        rgbout = self.RGBStream(rgb)
+        #rgbout = self.rgbdropout(rgbout)
+        feat = rgbout
+        feat = feat.unsqueeze(1)
+        #feat = self.pool(feat)
+        out, self.h = self.lstm(feat, self.h)
+        return out.squeeze(1)
+
